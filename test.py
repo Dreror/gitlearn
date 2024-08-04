684213c65aa13c07dc70990d792007f16b573755
Dreror <lagutams@gmail.com>
2024-08-04 13:35:48 +0100

    change code

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+number_1 = int(input("input number1-> "))
+number_2 = int(input("input number2-> "))
 
 
 def add_two_num(num1, num2):
@@ -6,3 +8,4 @@ def add_two_num(num1, num2):
 
 def minus_two_number(num1, num2):
     return num1 - num2
+
